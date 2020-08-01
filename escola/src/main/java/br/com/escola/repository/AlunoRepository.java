b6c8a5b02819d8207db41a557bca234fdfdc85e7
unknown <icaroduartec@gmail.com>
2020-07-31 21:19:01 -0300

    Relacionamento de tabelas - SpringBoot

---
+++ escola/src/main/java/br/com/escola/repository/AlunoRepository.java
@@ -0,0 +1,15 @@
+package br.com.escola.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.escola.model.Aluno;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+	public List<Aluno> findAllByNomeContainingIgnoreCase(String nome);
+
+}
